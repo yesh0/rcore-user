f11b26b010a431c29a1b3e15d0e69ec9e4aa115d
yhl1219 <yhliu2000@outlook.com>
2022-06-09 23:49:28 +0800

    [update]

--- b.sh
+++ b.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-cd ucore/build; make -j; cd ../..
+mkdir -p ucore/build
+cd ucore/build; cmake .. -DARCH=riscv64; make -j; cd ../..
 cp ucore/build/riscv64/bpf_test build/riscv64/
 cp ucore/build/riscv64/bmonitor build/riscv64/
 cd bpf-samples; make; cd ..
-cp bpf-samples/*.o build/riscv64
+cp bpf-samples/*.o build/riscv64/
 make sfsimg PREBUILT=1 ARCH=riscv64
