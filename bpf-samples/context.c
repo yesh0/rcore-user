9bea1d492e54615f38a9c72de92bd3b26cba026c
yhl1219 <yhliu2000@outlook.com>
2022-06-10 08:36:18 +0800

    [context] better format for context

--- bpf-samples/context.c
+++ bpf-samples/context.c
@@ -2,11 +2,24 @@
 #include "kprobe.h"
 
 int bpf_prog(struct kprobe_bpf_ctx *ctx) {
-  bpf_trace_printk("probe type = {}", ctx->ptype, 0, 0);
-  bpf_trace_printk("probe addr = {}", ctx->paddr, 0, 0);
+  // report tracepoint type
+  if (ctx->ptype == 0)
+    bpf_trace_printk("kprobe\n", 0, 0, 0);
+  else if (ctx->ptype == 1)
+    bpf_trace_printk("kretprobe@entry\n", 0, 0, 0);
+  else
+    bpf_trace_printk("kretprobe@exit\n", 0, 0, 0);
 
-  for (int i = 0; i < 32; ++i) {
-    bpf_trace_printk("register {} = {}", i, ctx->tf.general[i], 0);
+  // report tracepoint address
+  bpf_trace_printk("\taddr = {:x}\n", ctx->paddr, 0, 0);
+
+  // report registers
+  for (int i = 0; i < 8; ++i) {
+    for (int j = 0; j < 4; ++j) {
+      int reg = j + i * 4;
+      bpf_trace_printk("r{} {:x}", reg, ctx->tf.general[reg], 0);
+    }
+    bpf_trace_printk("\n", 0, 0, 0);
   }
 
   return 0;
