c74d6101e3b02db4fae5a6ecd3b92b41c1f0d148
yhl1219 <yhliu2000@outlook.com>
2022-06-10 08:41:33 +0800

    [context] better format

--- bpf-samples/context.c
+++ bpf-samples/context.c
@@ -1,6 +1,11 @@
 #include "bpf.h"
 #include "kprobe.h"
 
+static const char *reg_names[] = {
+    "zero", "ra", "sp", "gp", "tp",  "t0",  "t1", "t2", "s0", "s1", "a0",
+    "a1",   "a2", "a3", "a4", "a5",  "a6",  "a7", "s2", "s3", "s4", "s5",
+    "s6",   "s7", "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"};
+
 int bpf_prog(struct kprobe_bpf_ctx *ctx) {
   // report tracepoint type
   if (ctx->ptype == 0)
@@ -11,15 +16,14 @@ int bpf_prog(struct kprobe_bpf_ctx *ctx) {
     bpf_trace_printk("kretprobe@exit\n", 0, 0, 0);
 
   // report tracepoint address
-  bpf_trace_printk("\taddr = {:x}\n", ctx->paddr, 0, 0);
+  bpf_trace_printk("\taddr = {}\n", ctx->paddr, 0, 0);
 
   // report registers
-  for (int i = 0; i < 8; ++i) {
-    for (int j = 0; j < 4; ++j) {
-      int reg = j + i * 4;
-      bpf_trace_printk("r{} {:x}", reg, ctx->tf.general[reg], 0);
-    }
-    bpf_trace_printk("\n", 0, 0, 0);
+  for (int i = 0; i < 32; ++i) {
+    bpf_trace_printk("r{}", i, 0, 0);
+    if (i < 10)
+      bpf_trace_printk(" ", 0, 0, 0);
+    bpf_trace_printk(" = {}\n", ctx->tf.general[i], 0, 0);
   }
 
   return 0;
