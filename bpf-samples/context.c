1f930f8cddcaa79edaa63926cab737889f8d6f9a
yhl1219 <yhliu2000@outlook.com>
2022-06-10 08:46:40 +0800

    [context] try different location of string

--- bpf-samples/context.c
+++ bpf-samples/context.c
@@ -1,12 +1,6 @@
 #include "bpf.h"
 #include "kprobe.h"
 
-static const char *reg_names[] = {
-    "zero", "ra  ", "sp  ", "gp  ", "tp  ", "t0  ", "t1  ", "t2  ",
-    "s0  ", "s1  ", "a0  ", "a1  ", "a2  ", "a3  ", "a4  ", "a5  ",
-    "a6  ", "a7  ", "s2  ", "s3  ", "s4  ", "s5  ", "s6  ", "s7  ",
-    "s8  ", "s9  ", "s10 ", "s11 ", "t3  ", "t4  ", "t5  ", "t6  "};
-
 int bpf_prog(struct kprobe_bpf_ctx *ctx) {
   // report tracepoint type
   if (ctx->ptype == 0)
@@ -18,6 +12,12 @@ int bpf_prog(struct kprobe_bpf_ctx *ctx) {
 
   // report tracepoint address
   bpf_trace_printk("\taddr = {}\n", ctx->paddr, 0, 0);
+
+  const char *reg_names[] = {
+      "zero", "ra  ", "sp  ", "gp  ", "tp  ", "t0  ", "t1  ", "t2  ",
+      "s0  ", "s1  ", "a0  ", "a1  ", "a2  ", "a3  ", "a4  ", "a5  ",
+      "a6  ", "a7  ", "s2  ", "s3  ", "s4  ", "s5  ", "s6  ", "s7  ",
+      "s8  ", "s9  ", "s10 ", "s11 ", "t3  ", "t4  ", "t5  ", "t6  "};
 
   // report registers
   for (int i = 0; i < 32; ++i) {
