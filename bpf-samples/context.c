6a344a435d8465ed598b4feaeb1a69b42a1e4cfb
yhl1219 <yhliu2000@outlook.com>
2022-06-10 08:44:22 +0800

    [context] reg names for it

--- bpf-samples/context.c
+++ bpf-samples/context.c
@@ -2,9 +2,10 @@
 #include "kprobe.h"
 
 static const char *reg_names[] = {
-    "zero", "ra", "sp", "gp", "tp",  "t0",  "t1", "t2", "s0", "s1", "a0",
-    "a1",   "a2", "a3", "a4", "a5",  "a6",  "a7", "s2", "s3", "s4", "s5",
-    "s6",   "s7", "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"};
+    "zero", "ra  ", "sp  ", "gp  ", "tp  ", "t0  ", "t1  ", "t2  ",
+    "s0  ", "s1  ", "a0  ", "a1  ", "a2  ", "a3  ", "a4  ", "a5  ",
+    "a6  ", "a7  ", "s2  ", "s3  ", "s4  ", "s5  ", "s6  ", "s7  ",
+    "s8  ", "s9  ", "s10 ", "s11 ", "t3  ", "t4  ", "t5  ", "t6  "};
 
 int bpf_prog(struct kprobe_bpf_ctx *ctx) {
   // report tracepoint type
@@ -23,6 +24,9 @@ int bpf_prog(struct kprobe_bpf_ctx *ctx) {
     bpf_trace_printk("r{}", i, 0, 0);
     if (i < 10)
       bpf_trace_printk(" ", 0, 0, 0);
+    bpf_trace_puts("(");
+    bpf_trace_print_str(reg_names[i], 4);
+    bpf_trace_puts(")");
     bpf_trace_printk(" = {}\n", ctx->tf.general[i], 0, 0);
   }
 
