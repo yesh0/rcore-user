023140c43bdfa9f345890649ee64284b6fe20f73
yhl1219 <yhliu2000@outlook.com>
2022-06-10 10:11:59 +0800

    [context] add return value

--- bpf-samples/context.c
+++ bpf-samples/context.c
@@ -18,7 +18,20 @@ int bpf_prog(struct kprobe_bpf_ctx *ctx) {
     bpf_trace_printk("r{}", i, 0, 0);
     if (i < 10)
       bpf_trace_printk(" ", 0, 0, 0);
-    bpf_trace_printk(" = {}\n", ctx->tf.general[i], 0, 0);
+    bpf_trace_printk(" = {}\n", ctx->tf.regs[i], 0, 0);
+  }
+
+  // recover return result from a0
+  char *ret_addr = (char *)ctx->tf.general.a0;
+  char enum_flag = *ret_addr;
+  if (enum_flag == 0) {
+    size_t retvalue = *(size_t *)(ret_addr + 8);
+    bpf_trace_printk("ret = Ok({})\n", retvalue, 0, 0);
+  } else if (enum_flag == 1) {
+    size_t syserror_flag = *(size_t *)(ret_addr + 8);
+    bpf_trace_printk("ret = Err({})\n", syserror_flag, 0, 0);
+  } else {
+    bpf_trace_printk("failed to fetch SysResult return value\n", 0, 0, 0);
   }
 
   return 0;
